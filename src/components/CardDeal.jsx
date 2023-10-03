dfbb722a87bd2a44fc6e9d0538a667d9ca012ebf
Cédric RENAIS <cedric.renais@gmail.com>
2023-10-03 17:10:24 +0200

    v0.009: Création CardDeal section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
 
-const CardDeal = () => {
-  return <div>CardDeal</div>;
-};
+const FeedbackCard = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Trouvez la meilleure offre
+        <br className="sm:block hidden" /> en quelques étapes.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Avec notre large selection de cartes, vous trouverez la carte qui
+        repondra le mieux à vos besoins.
+      </p>
+      <Button styles="mt-10" />
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="Services de cartes" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
 
-export default CardDeal;
+export default FeedbackCard;
